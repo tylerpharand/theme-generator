ff85e6d5a88b44b8fdc89fe56849881197b1c43c
Tyler <tylerpharand@gmail.com>
2020-11-15 23:02:19 -0500

    feature: Build pipline

---
+++ themes/moonlight/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+BUILD_DIR="./tmp"
+CONTROL_TEMPLATE="./templates/control.template"
+INFO_PLIST_TEMPLATE="./templates/Info.plist.template"
+
+# Load config
+source "./config"
+
+# Export icons as PNG
+echo "🎨 Exporting icons..."
+# osascript ./scripts/export-artboards.scpt
+
+# Build theme
+echo "🔨 Building theme (${THEME_NAME})..."
+if [ -d "$BUILD_DIR" ]; then rm -Rf $BUILD_DIR; fi
+
+WORK_DIR="$BUILD_DIR/$THEME_NAME"
+mkdir $BUILD_DIR
+mkdir $WORK_DIR
+mkdir "$WORK_DIR/DEBIAN"
+eval "echo \"$(< $CONTROL_TEMPLATE)\"" > "$WORK_DIR/DEBIAN/control"
+
+mkdir "$WORK_DIR/Library"
+mkdir "$WORK_DIR/Library/Themes"
+THEME_DIR="$WORK_DIR/Library/Themes/${THEME_NAME}.theme"
+mkdir $THEME_DIR
+mkdir "$THEME_DIR/IconBundles"
+mkdir "$THEME_DIR/Bundles"
+# TODO: Generate icon masks of varying dimensions (?)
+mkdir "$THEME_DIR/Bundles/com.apple.mobileicons.framework"
+cp -r "./src/icons/" "$THEME_DIR/IconBundles"
+eval "echo \"$(< $INFO_PLIST_TEMPLATE)\"" > "$THEME_DIR/Info.plist"
+
+dpkg -b $WORK_DIR
+cp "$WORK_DIR.deb" "./build/${THEME_NAME}_$VERSION.deb"
+
+# Tidy up
+rm -r $BUILD_DIR
+
+# Complete
+echo "Saved package to "./build/moonlight_v0.1.0""
+echo "✨ Done!"
---
+++ themes/moonlight/scripts/export-artboards.jsx
@@ -0,0 +1,20 @@
+var docRef = app.activeDocument;
+
+function exportArtboards() {
+    for (var i = 0; i < docRef.artboards.length; i++) {
+        var artboardName = docRef.artboards[i].name;
+
+        // Skip artboards that start with "Artboard" or that start with a dash (-)
+        if (!(artboardName.match(/^artboard/) || artboardName.match(/^\-/))) {
+            var filename = "~/Desktop/theme-generator/themes/moonlight/src/icons/" + artboardName + ".png"
+            var destFile = new File(filename);
+            var options = new ExportOptionsPNG24();
+            options.artBoardClipping = true;
+            options.matte = true;
+            docRef.artboards.setActiveArtboardIndex(i);
+            docRef.exportFile(destFile, ExportType.PNG24, options);
+        }
+    }
+}
+
+exportArtboards()
